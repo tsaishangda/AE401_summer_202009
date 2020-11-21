1fc142ce5244457cefbf3d0e0e94e8200f20d83a
tsaishangda <70463175+tsaishangda@users.noreply.github.com>
2020-11-21 11:57:08 +0800

    Add files via upload

--- f.py
+++ f.py
@@ -1,7 +1,13 @@
 from mcpi.minecraft import Minecraft
 mc =Minecraft.create()
 
-x,y,z = mc.player.getTilePos()
+def plantTree(x,y,z):
+    mc.setBlocks(x,y+3,z,x+2,y+2+3,z+2,18)
+    mc.setBlocks(x+1,y,z+1,x+1,y+4,z+1,17)
 
-mc.setBlocks(x,y+3,z,x+2,y+2+3,z+2,18)
-mc.setBlocks(x+1,y,z+1,x+1,y+4,z+1,17)
+a,b,c = mc.player.getTilePos()
+
+for i in range(10):
+    for j in range(10):
+        for k in range(10):    
+            plantTree(a+i*5,b+j*i,c+k*i)
